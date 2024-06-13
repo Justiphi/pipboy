70380270e57871222ddebcee25d29c86268d9938
Travis Wylie <traviswylie58@gmail.com>
2024-06-13 12:57:11 +1200

    revert

--- pypboy/__init__.py
+++ pypboy/__init__.py
@@ -160,8 +160,6 @@ class SubModule(game.EntityGroup):
         self.paused = True
 
     def handle_resume(self):
-        if self.paused == False:
-            return
         self.paused = False
         if config.SOUND_ENABLED:
             print("Resume Sound")
